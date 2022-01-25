850363ae0399d9e77f77d6beec9e375de150b823
Cristi2002 <cristianistrate1234@gmail.com>
2022-01-25 23:35:55 +0300

    Noi modificari

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
+
 let sidebarBox = document.querySelector('.box'),
     sidebarBtn = document.querySelector('.btn'),
     pageWrapper = document.querySelector('.page-wrapper');
+
+
+
+let htmlElements = "";
+for (let i = 0; i < 7; i++) {
+    htmlElements += '<div class="item">List Item</div>';
+}
+let container = document.getElementById("container");
+container.innerHTML = htmlElements;
 
 sidebarBtn.addEventListener('click', event => {
     sidebarBtn.classList.toggle('active');
