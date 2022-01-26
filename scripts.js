7eff75d308aac9d3e2d08f67fa664279c6298c02
Cristi2002 <cristianistrate1234@gmail.com>
2022-01-26 20:46:51 +0300

    Modificari Noi la Proiect

--- scripts.js
+++ scripts.js
@@ -1,21 +1,24 @@
-let sidebarBox = document.querySelector('.box'),
-    sidebarBtn = document.querySelector('.btn'),
+let sidebarBox = document.querySelector('.sideMenuBox'),
+    sidebarBtn = document.querySelector('.buttonMenu'),
     pageWrapper = document.querySelector('.page-wrapper');
+    divItemsBox = document.querySelector('.divItemsBox');
 
-let htmlElements = "";
-for (let i = 0; i < 7; i++) {
-    htmlElements += '<div class="item">List Item</div>';
-}
-let container = document.getElementById("container");
-container.innerHTML = htmlElements;
+const listItem = ['List Item 01' , 'List Item 02' , 'List Item 03' ,'List Item 04' ,
+    'List Item 05' , 'List Item 06' , 'List Item 07' , 'List Item 08'];
 
-sidebarBtn.addEventListener('click', event => {
+listItem.forEach((e) =>{
+   let div = document.createElement('div');
+    divItemsBox.appendChild(div);
+    div.setAttribute('class' , 'listItem');
+    div.innerHTML = e;
+})
+
+sidebarBtn.addEventListener('click', (e) => {
     sidebarBtn.classList.toggle('active');
     sidebarBox.classList.toggle('active');
 });
 
-pageWrapper.addEventListener('click', event => {
-
+pageWrapper.addEventListener('click', (e) => {
     if (sidebarBox.classList.contains('active')) {
         sidebarBtn.classList.remove('active');
         sidebarBox.classList.remove('active');
@@ -23,7 +26,6 @@ pageWrapper.addEventListener('click', event => {
 });
 
 window.addEventListener('keydown', event => {
-
     if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
         sidebarBtn.classList.remove('active');
         sidebarBox.classList.remove('active');
